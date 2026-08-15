81d009b899df2b63f7ff88e1b1e1385fac02ab42
Ankitojha-air <onmanojojha@gmail.com>
2026-08-16 01:35:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (9.44%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -7,7 +7,7 @@ public:
         vector<vector<int>> arr(n, vector<int>(m));
 
         for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
+            for(int j = i + 1; j < m; j++) {
                 swap(nums[i][j],nums[j][i]);
             }
         }
